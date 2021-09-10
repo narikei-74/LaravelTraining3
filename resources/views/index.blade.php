93ecc08e1cc3f36399bfac0eb9e9c5abcce926a5
narikei_74 <uc9pr8cztud79bqm8g2v@docomo.ne.jp>
2021-09-10 16:50:46 +0900

    ドットインストールに沿ってデータベースの内容を表示させるトレーニングをマスターしました。

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<x-layout>
+    <x-slot name="title">
+        My BBS
+    </x-slot>
+
+    <h1>My BBS</h1>
+    <ul>
+        @forelse ($posts as $post)
+            <li>
+                <a href="{{ route('posts.show', $post->id) }}">{{ $post->title }}</a>
+            </li>
+        @empty
+            <li>No posts yet!</li>
+        @endforelse
+    </ul>
+</x-layout>
+
